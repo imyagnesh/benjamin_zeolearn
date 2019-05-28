b740f2dec85a42e5af95ff99b437610ed0acd12c
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-05-28 22:33:45 +0530

    comit api code

--- my-app/src/Screens/Home/index.js
+++ my-app/src/Screens/Home/index.js
@@ -1,15 +1,79 @@
 import React, { Component } from 'react';
+import { API } from '../../utils';
 // import PropTypes from 'prop-types';
 
 export default class index extends Component {
   static propTypes = {};
 
-  state = {};
+  state = {
+    courses: [],
+    authors: [],
+    loading: false,
+    error: false,
+  };
+
+  componentDidMount() {
+    this.fetchAPI();
+  }
+
+  fetchAPI = async () => {
+    this.setState({ loading: true });
+    try {
+      const coursesAPI = API({ uri: 'http://localhost:3004/courses' });
+      const authorsAPI = API({ uri: 'http://localhost:3004/authors' });
+      const data = await Promise.all([coursesAPI, authorsAPI]);
+      this.setState({ courses: data[0], authors: data[1], loading: false });
+    } catch (error) {
+      this.setState({ loading: false, error: 'Oops! something went wrong' });
+    }
+  };
+
+  renderAuthor = id => {
+    const { authors } = this.state;
+    const author = authors.find(x => x.id === id);
+    if (author) {
+      return `${author.firstName} ${author.lastName}`;
+    }
+    return '';
+  };
 
   render() {
+    const { courses, loading, error } = this.state;
+    if (loading) {
+      return <h1>Loading...</h1>;
+    }
+
+    if (error) {
+      return <h1>{error}</h1>;
+    }
+
     return (
       <div>
         <h1>Home Page</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Link</th>
+              <th>Author</th>
+              <th>Length</th>
+              <th>Category</th>
+            </tr>
+          </thead>
+          <tbody>
+            {courses.map(course => (
+              <tr key={course.id}>
+                <td>{course.title}</td>
+                <td>
+                  <a href={course.watchHref}>Link</a>
+                </td>
+                <td>{this.renderAuthor(course.authorId)}</td>
+                <td>{course.length}</td>
+                <td>{course.category}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     );
   }
---
+++ my-app/src/utils.js
@@ -0,0 +1,26 @@
+export const data = 1;
+
+export const API = ({ uri, method = 'GET', header = {}, body = null }) => {
+  let options = {
+    method,
+    headers: {
+      accept: 'application/json',
+      'Content-Type': 'application/json',
+      ...header,
+    },
+  };
+
+  if (body) {
+    options = { ...options, body: JSON.stringify(body) };
+  }
+
+  return new Promise(async (resolve, reject) => {
+    try {
+      const res = await fetch(uri, options);
+      const json = await res.json();
+      resolve(json);
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
